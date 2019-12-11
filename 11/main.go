025615e68334cb209585bc1add748623de808263
Chris Smith <chris@chameth.com>
2019-12-11 17:29:56 +0000

    Tidy day 11

--- 11/main.go
+++ 11/main.go
@@ -4,9 +4,22 @@ import (
 	"fmt"
 	"github.com/csmith/aoc-2019/common"
 	"github.com/csmith/aoc-2019/intcode"
+	"math"
 )
 
-func run(memory []int, startWhite bool) (int, [500][500]bool) {
+type point struct {
+	x int
+	y int
+}
+
+var dirs = [4]point{
+	0: {y: -1},
+	1: {x: +1},
+	2: {y: +1},
+	3: {x: -1},
+}
+
+func run(memory []int, startWhite bool) (int, map[point]bool) {
 	input := make([]int, len(memory))
 	copy(input, memory)
 	vm := intcode.NewVirtualMachine(input)
@@ -14,17 +27,15 @@ func run(memory []int, startWhite bool) (int, [500][500]bool) {
 	vm.Output = make(chan int, 1)
 	go vm.Run()
 
-	var grid [500][500]bool
-	var painted [500][500]bool
-
+	grid := make(map[point]bool, 50)
 	paintCount := 0
-	x := 250
-	y := 250
+
+	pos := point{x: 0, y: 0}
 	dir := 0
-	grid[y][x] = startWhite
+	colour := startWhite
 
 	for {
-		if grid[y][x] {
+		if colour {
 			vm.Input <- 1
 		} else {
 			vm.Input <- 0
@@ -36,28 +47,15 @@ func run(memory []int, startWhite bool) (int, [500][500]bool) {
 		}
 		turn := <-vm.Output
 
-		grid[y][x] = paint == 1
-		if !painted[y][x] {
+		if _, ok := grid[pos]; !ok {
 			paintCount++
-			painted[y][x] = true
 		}
+		grid[pos] = paint == 1
 
-		if turn == 1 {
-			dir = (dir + 1) % 4
-		} else {
-			dir = (dir + 3) % 4
-		}
-
-		switch dir {
-		case 0:
-			y--
-		case 1:
-			x++
-		case 2:
-			y++
-		case 3:
-			x--
-		}
+		dir = (4 + dir + 2*turn - 1) % 4
+		pos.x += dirs[dir].x
+		pos.y += dirs[dir].y
+		colour = grid[pos]
 	}
 
 	return paintCount, grid
@@ -68,10 +66,18 @@ func main() {
 	paintCount, _ := run(input, false)
 	_, grid := run(input, true)
 
-	println(paintCount)
-	for _, line := range grid {
-		for _, b := range line {
-			if b {
+	fmt.Println(paintCount)
+
+	minX, minY := math.MaxInt64, math.MaxInt64
+	maxX, maxY := math.MinInt64, math.MinInt64
+	for pos := range grid {
+		minX, maxX = common.Min(minX, pos.x), common.Max(maxX, pos.x)
+		minY, maxY = common.Min(minY, pos.y), common.Max(maxY, pos.y)
+	}
+
+	for y := minY; y <= maxY; y++ {
+		for x := minX; x <= maxX; x++ {
+			if grid[point{x: x, y: y}] {
 				fmt.Print("█")
 			} else {
 				fmt.Print(" ")
--- common/math.go
+++ common/math.go
@@ -5,3 +5,19 @@ func Abs(x int64) int64 {
 	y := x >> 63
 	return (x ^ y) - y
 }
+
+// Max returns the highest of the two given ints.
+func Max(x, y int) int {
+	if x >= y {
+		return x
+	}
+	return y
+}
+
+// Min returns the lowest of the two given ints.
+func Min(x, y int) int {
+	if x <= y {
+		return x
+	}
+	return y
+}
